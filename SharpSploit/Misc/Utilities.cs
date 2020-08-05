d4d54c44a4b1be89769bf646420c0179ee6e7f9a
Julio Ureña <juliourena33@gmail.com>
2020-08-05 09:32:36 -0400

    Update Utilities.cs
    Updated to support PassTheHash

--- SharpSploit/Misc/Utilities.cs
+++ SharpSploit/Misc/Utilities.cs
@@ -1,4 +1,4 @@
-﻿// Author: Ryan Cobb (@cobbr_io)
+// Author: Ryan Cobb (@cobbr_io)
 // Project: SharpSploit (https://github.com/cobbr/SharpSploit)
 // License: BSD 3-Clause
 
@@ -8,6 +8,9 @@ using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 
 namespace SharpSploit.Misc
 {
@@ -108,6 +111,40 @@ namespace SharpSploit.Misc
         public static bool Is64Bit
         {
             get { return IntPtr.Size == 8; }
+        }
+
+        public static ushort DataLength(int length_start, byte[] string_extract_data)
+        {
+            byte[] bytes = { string_extract_data[length_start], string_extract_data[length_start + 1] };
+            ushort string_length = BitConverter.ToUInt16(GetByteRange(string_extract_data, length_start, length_start + 1), 0);
+            return string_length;
+        }
+        public static byte[] GetByteRange(byte[] array, int start, int end)
+        {
+            var newArray = array.Skip(start).Take(end - start + 1).ToArray();
+            return newArray;
+        }
+
+        public static byte[] ConvertStringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        public static byte[] ConvertFromPacketOrderedDictionary(OrderedDictionary packet_ordered_dictionary)
+        {
+            List<byte[]> byte_list = new List<byte[]>();
+            foreach (DictionaryEntry de in packet_ordered_dictionary)
+            {
+                byte_list.Add(de.Value as byte[]);
+            }
+
+            var flattenedList = byte_list.SelectMany(bytes => bytes);
+            byte[] byte_Array = flattenedList.ToArray();
+
+            return byte_Array;
         }
     }
 }
